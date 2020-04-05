897c51daa687570027f4071178fd86e2c7f4446f
Sviatlana Harchanina <sviatlana.harchanina@leverx.com>
2020-04-05 11:40:42 +0300

    Update routing.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,9 @@
 require('reflect-metadata');
 const express = require('express');
-const swaggerUI = require('swagger-ui-express');
-const path = require('path');
-const YAML = require('yamljs');
 const router = require('./router');
-
 const app = express();
-const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
-
-app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/', router);
 app.use((err, req, res, next) => {
   if (err) {
--- src/router.js
+++ src/router.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const path = require('path');
+const YAML = require('yamljs');
 const router = express.Router();
+const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
+router.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 router.use('/users', require('./resources/users/user.router'));
 
 module.exports = router;
