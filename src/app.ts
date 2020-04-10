3993dfdb7eb25fe6ef46a8f49993f4f5c45deb58
Sviatlana Harchanina <sviatlana.harchanina@leverx.com>
2020-04-10 10:33:05 +0300

    Add logger for uncaughtException and unhandledRejection.

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { router } from './router';
 import { responseLogger } from "./middlewares/responseLogger";
 import { errorLogger } from "./middlewares/errorLogger";
 import { errorHandler } from "./middlewares/errorHandler";
+import { LoggerService } from "./services/loggerService";
 
 export const app = express();
 
@@ -12,3 +13,7 @@ app.use(responseLogger);
 app.use('/', router);
 app.use(errorLogger);
 app.use(errorHandler);
+
+const logger = new LoggerService();
+process.on('uncaughtException', err => logger.logError(err));
+process.on('unhandledRejection', err => logger.logError(err));
